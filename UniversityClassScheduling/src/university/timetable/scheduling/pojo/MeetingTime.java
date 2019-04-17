bfba2539295c80b4375497a386b277b9afbbc216
NikitaGundyal <nikitagundyal13@gmail.com>
2019-04-17 13:16:23 -0400

    MeetingTime and Room pojo added

--- UniversityClassScheduling/src/university/timetable/scheduling/pojo/MeetingTime.java
+++ UniversityClassScheduling/src/university/timetable/scheduling/pojo/MeetingTime.java
@@ -1,5 +1,21 @@
-package university.timetable.scheduling.pojo;
+package com.neu.edu.doamin;
 
 public class MeetingTime {
-
+	
+	private String id;
+	private String time;
+	
+	public MeetingTime(String id, String time) {
+		this.id = id;
+		this.time = time;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public String getTime() {
+		return time;
+	}
+	
 }
