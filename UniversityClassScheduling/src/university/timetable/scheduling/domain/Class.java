7f90d6476543455faaea1f9fa80098c5e9b1efc0
Harshil Duahat <dudhat.h@husky.neu.edu>
2019-04-17 11:32:26 -0400

    pojo classes

--- UniversityClassScheduling/src/university/timetable/scheduling/domain/Class.java
+++ UniversityClassScheduling/src/university/timetable/scheduling/domain/Class.java
@@ -1,4 +1,56 @@
 package university.timetable.scheduling.domain;
 
 public class Class {
+
+    		this.course = course;
+}
+
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public void setMeetingTime(MeetingTime meetingTime) {
+        this.meetingTime = meetingTime;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+
+    public int getClassId() {
+        return classId;
+    }
+
+
+
+    public Department getDepartment() {
+        return department;
+    }
+
+
+    public Course getCourse() {
+        return course;
+    }
+
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+
+    public MeetingTime getMeetingTime() {
+        return meetingTime;
+    }
+
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public String toString() {
+        return department.getName()+", "+ course.getCRNumber()+", "+room.getRoomnumber()+", "+ instructor.getName()+", "+meetingTime.getId();
+
+    }
 }
--- UniversityClassScheduling/src/university/timetable/scheduling/pojo/Course.java
+++ UniversityClassScheduling/src/university/timetable/scheduling/pojo/Course.java
@@ -2,4 +2,39 @@ package university.timetable.scheduling.pojo;
 
 public class Course {
 
+    private String name =null;
+    private String CRNumber=null;
+    private int maxNumberOfStudents;
+    private ArrayList<Instructor> instructors;
+
+    public Course(String number, String name, int maxNumberOfStudents, ArrayList<Instructor> instructors) {
+
+        this.name = name;
+        this.CRNumber = number;
+        this.maxNumberOfStudents = maxNumberOfStudents;
+        this.instructors = instructors;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public String getCRNumber() {
+        return CRNumber;
+    }
+
+    public int getMaxNumberOfStudents() {
+        return maxNumberOfStudents;
+    }
+
+    public ArrayList<Instructor> getInstructors() {
+        return instructors;
+    }
+
+    public String toString() {
+        return name;
+
+    }
+
 }
